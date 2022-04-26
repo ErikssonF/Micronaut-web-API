85eb8b621803ee737357b4fb7ccdc2408f82e931
Fredrik Eriksson <eriksson.nf@gmail.com>
2022-04-26 15:14:48 +0200

    Added FruitController

---
+++ src/main/java/controller/FruitController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import dtos.Fruit;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Status;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+import repository.FruitRepository;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+import static io.micronaut.http.HttpStatus.CREATED;
+
+@Controller("/fruits")
+@ExecuteOn(TaskExecutors.IO)
+class FruitController {
+
+    private final FruitRepository fruitService;
+
+    FruitController(FruitRepository fruitService) {
+        this.fruitService = fruitService;
+    }
+
+    @Get
+    List<Fruit> list() {
+        return fruitService.list();
+    }
+
+    @Post
+    @Status(CREATED)
+    void save(@NonNull @NotNull @Valid Fruit fruit) {
+        fruitService.save(fruit);
+    }
+}
