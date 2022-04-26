2c2762c4bfd9a9d2f0e4e963a50c52c3be47e166
Fredrik Eriksson <eriksson.nf@gmail.com>
2022-04-26 15:13:48 +0200

    Added FruitRepository

---
+++ src/main/java/repository/FruitRepository.java
@@ -0,0 +1,16 @@
+package repository;
+
+import dtos.Fruit;
+import io.micronaut.core.annotation.NonNull;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+public interface FruitRepository {
+
+    @NonNull
+    List<Fruit> list();
+
+    void save(@NonNull @NotNull @Valid Fruit fruit);
+}
